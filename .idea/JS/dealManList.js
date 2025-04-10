c83b9725709b27052115ebbbcf936f2f06034330
Gushebush <rodionSokolov06@gmail.com>
2025-04-10 22:46:55 +0300

    update

--- .idea/JS/dealManList.js
+++ .idea/JS/dealManList.js
@@ -1,10 +1,12 @@
     "use strict"
-    GETPromise()
-        .then(result => {
-            addListItems(result);
-        });
-    const list = document.querySelector(".main__list");
-    function getFormattedDateTime() {
+const url = "https://6709508caf1a3998baa11eb3.mockapi.io/api/v1/todos";
+const TaskContainer = document.querySelector(".main__list");
+const addTaskButton  = document.querySelector(".main__confirm-button");
+const taskHeader= document.querySelector(".main__head-input");
+const taskDescription = document.querySelector(".main__main-text-input");
+const CurrentTime = getTime();
+let tasksArray = [];
+function getTime() {
         const now = new Date();
         const options = {
             weekday: 'short',
@@ -16,21 +18,25 @@
             second: '2-digit',
             timeZoneName: 'long'
         };
-        const dateTimeString = now.toLocaleString('en-US', options);
+        const dateTimeString = now.toLocaleString('ru', options);
         const timeZoneOffset = now.getTimezoneOffset();
         const gmtOffset = `GMT${timeZoneOffset > 0 ? '-' : '+'}${Math.abs(timeZoneOffset / 60).toString().padStart(2, '0')}00`;
         return `${dateTimeString} ${gmtOffset}`;
     };
-
-async function POST_GETpromise(object){
-    await POSTPromise(object)
-    await GETPromise()
-        .then(result => {
-            addListItems(result);
-        });
-}
-async function POSTPromise(object) {
-    let promise = await fetch("https://6709508caf1a3998baa11eb3.mockapi.io/api/v1/todos",{
+function addArrayTasks(json) {
+    for (let object of json) {
+        let arrayObject = {
+            id:     object.id,
+            title: object.title,
+            description: object.description,
+            done: object.done,
+            createdAt: object.createdAt,
+        }
+        tasksArray.push(arrayObject);
+    }
+};
+async function addTask(object) {
+    let promise = await fetch(url,{
         method: "POST",
         headers: {
             "Content-Type": "application/json"
@@ -38,74 +44,83 @@ async function POSTPromise(object) {
         body: JSON.stringify(object),
     });
 };
-async function GETPromise() {
+async function getTasks() {
     let promise = await fetch(`https://6709508caf1a3998baa11eb3.mockapi.io/api/v1/todos`);
     let json = await promise.json();
-    return json;
+    await addArrayTasks(json);
 };
-async function DELETEPromise(id) {
+async function deleteTask(id) {
     let promise = await fetch(`https://6709508caf1a3998baa11eb3.mockapi.io/api/v1/todos/${id}`, {
         method: "DELETE"
     });
 };
-function addListItems(json){
-    for (let objects of json){
-        let listItem = document.createElement('li');
-        listItem.classList.add(objects.id);
-        let listItemHeader = listItem.appendChild(document.createElement("h4"));
-        listItemHeader.textContent = objects.title;
-        let listItemMainText = listItem.appendChild(document.createElement("p"));
-        listItemMainText.textContent = objects.description;
-        let listItemCloseButton = listItem.appendChild(document.createElement("div"));
-        listItemCloseButton.className = "close-button";
-        listItemCloseButton.textContent = "x";
-        listItemCloseButton.addEventListener("click", ()=>{
-            listItemCloseButton.parentElement.remove();
-            let idItem = listItemCloseButton.parentElement.classList.value;
-            DELETEPromise(idItem);
-        });
-        list.appendChild(listItem);
-        };
+function createTask(object) {
+    let task = document.createElement('li');
+    task.classList.add(object.id);
+    let taskHeader = task.appendChild(document.createElement("h4"));
+    taskHeader.textContent = object.title;
+    let taskDescription = task.appendChild(document.createElement("p"));
+    taskDescription.textContent = object.description;
+    let taskCloseButton = task.appendChild(document.createElement("div"));
+    taskCloseButton.className = "close-button";
+    taskCloseButton.textContent = "x";
+    taskCloseButton.addEventListener("click", ()=>{
+        let taskId = taskCloseButton.parentElement.classList.value;
+        taskCloseButton.parentElement.remove();
+        deleteTask(taskId)
+            .then(() => {
+            tasksArray = tasksArray.filter((task) => task.id != taskId);
+        })
+            .catch((error) => {
+                console.log(error);
+            })
 
+    });
+    TaskContainer.appendChild(task);
 }
-function removeListItem(){
-    for (let lil of document.querySelectorAll('li')){
-        lil.remove();
+function renderTasks(json){
+    for (let objects of json){
+        createTask(objects);
+        };
+}
+function removeRenderedTasks(){
+    for (let li of document.querySelectorAll('li')){
+        li.remove();
     }
 }
-    let confirmButton  = document.querySelector(".main__confirm-button");
-confirmButton.addEventListener("click", function (event) {
-    let input= document.querySelector(".main__head-input");
-    let textarea = document.querySelector(".main__main-text-input");
-    let inputValue = input.value;
-    let textareaValue = textarea.value;
-    let data = getFormattedDateTime();
-    if (inputValue) {
-        let ListItemObject = {
-            createdAt: data,
-            title: inputValue,
-            description: textareaValue,
-            done: true,
+getTasks().then(result => {
+    renderTasks(tasksArray);
+});
+addTaskButton.addEventListener("click", function (event) {
+    const taskHeaderValue = taskHeader.value;
+    const taskDescriptionValue = taskDescription.value;
+    if (taskHeaderValue) {
+        let taskObject = {
+            id: ""+ (+tasksArray.at(-1).id + 1),
+            createdAt: CurrentTime,
+            title: taskHeaderValue,
+            description: taskDescriptionValue,
+            done: false,
         };
-        input.value = '';
-        textarea.value = '';
-        removeListItem();
-        POST_GETpromise(ListItemObject);
+        taskHeader.value = '';
+        taskDescription.value = '';
+        createTask(taskObject);
+        addTask(taskObject)
+            .then(result => {
+                tasksArray.push(taskObject);
+            })
+            .catch((error) => {
+                console.log(error);
+            })
     };
 });
 document.querySelector(".sort-to-lower-button").addEventListener("click", function (event) {
-    removeListItem();
-    GETPromise()
-        .then(result => {
-            result.sort(function (a, b) {return a.title.length - b.title.length});
-            addListItems(result);
-        })
+    removeRenderedTasks();
+    tasksArray.sort(function (a, b) {return a.title.length - b.title.length});
+    renderTasks(tasksArray);
 });
 document.querySelector(".sort-to-higher-button").addEventListener("click", function (event) {
-    removeListItem();
-    GETPromise()
-        .then(result => {
-            result.sort(function (a, b) {return b.title.length - a.title.length});
-            addListItems(result);
-        });
+    removeRenderedTasks();
+    tasksArray.sort(function (a, b) {return b.title.length - a.title.length});
+    renderTasks(tasksArray);
 });
